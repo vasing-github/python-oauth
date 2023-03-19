edc130f48759ec352439331c29d7d94302ddab96
VasingHome <vasing home@qq.com>
2023-03-19 21:48:45 +0800

    modify gui

--- tk3.py
+++ tk3.py
@@ -1,13 +1,9 @@
 
 import tkinter as tk
 from tkinter import *
-import io
-import sys
 from tkinter import scrolledtext
-import asyncio
 import threading
 import queue
-
 import 录件
 
 # 创建一个队列用于通信
@@ -17,48 +13,60 @@ q = queue.Queue()
 root = tk.Tk()
 root.title("一体化平台自动录件系统")
 
-# 修改窗口背景颜色
-root.configure(bg="#F0F0F0")
+
 
 # 第一行：输入框，提示用户输入 key
-key_label = tk.Label(root, text="Key:", font=("Arial", 14), bg="#F0F0F0")
-key_label.grid(row=0, column=0, padx=10, pady=10)
-key_entry = tk.Entry(root, font=("Arial", 24))
-key_entry.grid(row=0, column=1, padx=10, pady=10)
+key_label = tk.Label(root, text="Key:", font=("Arial", 12), bg="#F0F0F0")
+key_label.grid(row=0, column=0, padx=10, pady=10, sticky='w')
+key_entry = tk.Entry(root, font=("Arial", 12), width=50)
+key_entry.grid(row=0, column=1, padx=10, pady=10,  sticky='w', columnspan=2)
+record_button = tk.Button(root, text="获取key", font=("Arial", 12))
+record_button.grid(row=0, column=3, sticky='w', padx=10, pady=10)
 
 # 第二行：多个输入框，提示用户输入录件条数、推送条数、评价条数
-record_label = tk.Label(root, text="录件条数:", font=("Arial", 14), bg="#F0F0F0")
-record_label.grid(row=1, column=0, padx=10, pady=10)
-record_entry = tk.Entry(root, font=("Arial", 14))
-record_entry.grid(row=1, column=1, padx=10, pady=10)
+record_label = tk.Label(root, text="1.录件条数:", font=("Arial", 12), bg="#F0F0F0")
+record_label.grid(row=1, column=0, padx=10, pady=10, sticky='w')
+record_entry = tk.Entry(root, font=("Arial", 12), width = 10)
+record_entry.grid(row=1, column=1, padx=10, pady=10, sticky='w')
+record_button = tk.Button(root, text="开始录件", font=("Arial", 12))
+record_button.grid(row=1, column=2,padx=10, pady=10, sticky='w')
 
-push_label = tk.Label(root, text="推送条数:", font=("Arial", 14), bg="#F1F1F1")
-push_label.grid(row=1, column=2, padx=10, pady=10)
-push_entry = tk.Entry(root, font=("Arial", 14))
-push_entry.grid(row=1, column=3, padx=10, pady=10)
+# 第三行：多个输入框，提示用户输入录件条数、推送条数、评价条数
+shencha_button = tk.Button(root, text="2.1推送待审查", font=("Arial", 12))
+shencha_button.grid(row=2, column=0,padx=10, pady=10, sticky='w')
+jueding_button = tk.Button(root, text="2.2推送待决定", font=("Arial", 12))
+jueding_button.grid(row=2, column=1,padx=10, pady=10, sticky='w')
+zhizheng_button = tk.Button(root, text="2.3推送待制证", font=("Arial", 12))
+zhizheng_button.grid(row=2, column=2,padx=10, pady=10, sticky='w')
+fazheng_button = tk.Button(root, text="2.4推送待发证", font=("Arial", 12))
+fazheng_button.grid(row=2, column=3,padx=10, pady=10, sticky='w')
 
-review_label = tk.Label(root, text="评价条数:", font=("Arial", 14), bg="#F0F0F0")
-review_label.grid(row=1, column=4, padx=10, pady=10)
-review_entry = tk.Entry(root, font=("Arial", 14))
-review_entry.grid(row=1, column=5, padx=10, pady=10)
+# 第四行：评价相关
+review_label = tk.Label(root, text="3.评价条数:", font=("Arial", 12), bg="#F0F0F0")
+review_label.grid(row=3, column=0, padx=10, pady=10, sticky='w')
+review_entry = tk.Entry(root, font=("Arial", 12),width = 10)
+review_entry.grid(row=3, column=1, padx=10, pady=10, sticky='w')
+review_button = tk.Button(root, text="3.1系统件评价", font=("Arial", 12))
+review_button.grid(row=3, column=2, sticky='w', padx=10, pady=10)
+daorupingjia_button = tk.Button(root, text="3.2导入件评价", font=("Arial", 12))
+daorupingjia_button.grid(row=3, column=3, sticky='w', padx=10, pady=10)
 
-# 第三行：几个并排的按钮，分别是录件、推送、评价
-record_button = tk.Button(root, text="录件", font=("Arial", 14))
-record_button.grid(row=2, column=0)
+# 第五行：暂存件
+zancun_button = tk.Button(root, text="4.暂存件清理", font=("Arial", 12))
+zancun_button.grid(row=4, column=0, sticky='w', padx=10, pady=10)
 
-push_button = tk.Button(root, text="推送", font=("Arial", 14))
-push_button.grid(row=2, column=1)
-
-review_button = tk.Button(root, text="评价", font=("Arial", 14))
-review_button.grid(row=2, column=2)
-
+# 第6行：create a text widget to display the output
+text = scrolledtext.ScrolledText(root)
+text.grid(row=5, column=0, sticky='w', columnspan=3, padx=10, pady=10,rowspan=5)
+notice_label = tk.Label(root, text="注意:本软件仅作为学习使用，目的在于减少人工录入的错误，节约人工录入时间.\n\n本软件不会给服务器造成压力.\n\n录入数据需按真实情况填写在imEx.xlsx表格中，本软件不提供数据来源", font=("Arial", 12), bg="#F0F0F0",wraplength=150)
+notice_label.grid(row=5, column=3, padx=10, pady=10, sticky='w', columnspan=4,rowspan=5)
 def on_record_button_click():
 
     # 获取输入框中的值
     key = key_entry.get()
     record_count = record_entry.get()
-    push_count = push_entry.get()
-    review_count = review_entry.get()
+    # push_count = push_entry.get()
+    # review_count = review_entry.get()
 
 
     # 禁用按钮，避免重复启动任务
@@ -67,10 +75,7 @@ def on_record_button_click():
     worker_thread = threading.Thread(target=录件.run, args=(q,))
     worker_thread.start()
 
-# 第四行：create a text widget to display the output
 
-text = scrolledtext.ScrolledText(root)
-text.grid(row=3, column=1)
 
 # 定义一个函数用于更新GUI
 def update():
